22d569b9296ca23939c7f634605a96b270b19271
Patrick Morpey <patrick@morphp.nl>
2023-05-04 09:48:53 +0200

    Replaced placeholders

--- src/ObjectToArraySerializerPackage.php
+++ src/ObjectToArraySerializerPackage.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Medas\Placeholder;
+namespace Medas\ObjectToArraySerializer;
 
 use Medas\Core\AsSingleton;
 use Medas\ServiceManager\BasePackage;
 
-class PlaceholderPackage extends BasePackage
+class ObjectToArraySerializerPackage extends BasePackage
 {
     use AsSingleton;
 
