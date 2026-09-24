384ffb61882536d87fef327e6fb32484b266d9d0
Patrick Morpey <patrick@morphp.nl>
2026-09-24 15:46:27 +0200

    Updated `ArraysMustSpecifyContentType` exception to include class name in error messages for better context.

--- src/Exceptions/ArraysMustSpecifyContentType.php
+++ src/Exceptions/ArraysMustSpecifyContentType.php
@@ -10,11 +10,11 @@ class ArraysMustSpecifyContentType extends BaseException
 {
     public function __construct(\ReflectionProperty $property)
     {
-        parent::__construct($property->getName());
+        parent::__construct($property->getName(), $property->getDeclaringClass()->name);
     }
 
     public function pattern(): string
     {
-        return 'array property %s must specify the type of its content in its doccomment as "@var TypeName[]" or "@var array<TypeName>"';
+        return 'array property %s of class %s must specify the type of its content in its doccomment as "@var TypeName[]" or "@var array<TypeName>"';
     }
 }
